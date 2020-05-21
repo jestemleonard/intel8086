a46cbe181d43e755987f3d47832e545cdc30c880
Leonard Ulman <namludranoel@gmail.com>
2020-05-21 13:40:16 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Intel8086;
+
+namespace intel8086
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AssignValues();
+        }
+
+        private void executeButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (methods.SelectedOperation)
+            {
+                case 0:
+                    methods.MOV();
+                    break;
+                case 1:
+                    methods.SWAP();
+                    break;
+                case 2:
+                    methods.ADD();
+                    break;
+                case 3:
+                    methods.SUB();
+                    break;
+                default:
+                    MessageBox.Show("Proszę wybrać operację");
+                    break;
+            }
+
+            AssignValues();
+        }
+
+        private void listOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            methods.SelectedOperation = listOperation.SelectedIndex;
+        }
+        private void listFirstRegister_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            methods.SelectedFirstRegister = listFristRegister.SelectedIndex;
+        }
+        private void listSecondRegister_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            methods.SelectedSecondRegister = listSecondRegister.SelectedIndex;
+        }
+
+        private void AssignValues()
+        {
+            AX_TextBlock.Text = "AX #" + methods.AX.ToString("x");
+            BX_TextBlock.Text = "BX #" + methods.BX.ToString("x");
+            CX_TextBlock.Text = "CX #" + methods.CX.ToString("x");
+            DX_TextBlock.Text = "DX #" + methods.DX.ToString("x");
+        }
+    }
+}
---
+++ methods.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Intel8086
+{
+    class methods
+    {
+        //wartosci rejestrów
+        public static int SelectedOperation = -1;
+        public static int SelectedFirstRegister = -1;
+        public static int SelectedSecondRegister = -1;
+        public static ushort AX = 0x2B67; //11111
+        public static ushort BX = 0x56CE; //22222
+        public static ushort CX = 0x8235; //33333
+        public static ushort DX = 0xAD9C; //44444
+
+        /// <summary>
+        /// Zwraca wskaznik do rejestru
+        /// </summary>
+        /// <param name="register"></param>
+        /// <returns></returns>
+        public static ref ushort GetPointer(int register)
+        {
+            switch (register)
+            {
+                case 0:
+                    return ref AX;
+                case 1:
+                    return ref BX;
+                case 2:
+                    return ref CX;
+                case 3:
+                    return ref DX;
+                default:
+                    MessageBox.Show("Proszę wybrać rejestr");
+                    return ref AX;
+            }
+        }
+
+        /// <summary>
+        /// Kopiuje pierwszy rejestr w miejsce drugiego
+        /// </summary>
+        public static void MOV()
+        {
+            GetPointer(SelectedSecondRegister) = GetPointer(SelectedFirstRegister);
+        }
+        /// <summary>
+        /// Zmienia wartości pierwszego i drugiego rejestru
+        /// </summary>
+        public static void SWAP()
+        {
+            ushort c = GetPointer(SelectedFirstRegister);
+            GetPointer(SelectedFirstRegister) = GetPointer(SelectedSecondRegister);
+            GetPointer(SelectedSecondRegister) = c;
+        }
+        /// <summary>
+        /// Dodaje do pierwszego rejestru wartosc drugiego rejestru
+        /// </summary>
+        public static void ADD()
+        {
+            GetPointer(SelectedFirstRegister) += GetPointer(SelectedSecondRegister);
+        }
+        /// <summary>
+        /// Odejmuje od pierwszego rejestru wartosc drugiego rejestru
+        /// </summary>
+        public static void SUB()
+        {
+            GetPointer(SelectedFirstRegister) -= GetPointer(SelectedSecondRegister);
+        }
+    }
+}
